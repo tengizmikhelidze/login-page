5cea8c101b90bbc4114f0d95e3ccf340a0a2c445
10GO <mixelidzet@gmail.com>
2025-01-22 06:46:17 -0800

    feaet

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {Component} from '@angular/core';
+import {MatFormField} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {NgOptimizedImage} from '@angular/common';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   imports: [
     MatFormField,
     MatInput,
-    NgOptimizedImage
+    NgOptimizedImage,
+    MatCheckbox,
+    MatButton
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
